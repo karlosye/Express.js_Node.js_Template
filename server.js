5195487e09c829e5eb5148b4e35252fa46588745
Karlos <karlosye@live.com>
2021-12-21 16:20:22 -0500

    Add 'public' package setting.

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Set up the 'https' package:
 const https = require('https');
 
+// Set up the 'public' package to load static stuff like CSS and images
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
     res.send('<h1>Hello World.</h1>');
